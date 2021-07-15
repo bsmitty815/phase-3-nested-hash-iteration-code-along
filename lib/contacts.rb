8249a26e2fc3bf4cd2c5277c84d67f125fef720b
Brian Smith <brian.bubbam@gmail.com>
2021-07-15 14:17:42 -0500

    commit tree 0854c983ecfc2ba6e71c90be3caed725913a531d

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,14 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash| # each through the data of the contact which would be jon and freddy
+    #we are passing throught the person as the name, and contact_detals_hash is all of the info in the hash
+    if person == "Freddy Mercury" #one we get to freddy run the next each
+      contact_details_hash.each do |attribute, data| #the attribute is the key and data is the value
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"} #if there is strawberry we will delete it
+        end
+      end
+    end
+  end
 end
